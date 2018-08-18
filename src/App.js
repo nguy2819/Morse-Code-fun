7e8996608d9e5beed4921371f78fd9f0e0a568e0
Tien Borland <nguy2819@crk.umn.edu>
2018-08-18 15:06:38 -0600

    added some CSS

--- src/App.js
+++ src/App.js
@@ -1,85 +1,61 @@
 import React, { Component } from 'react';
+import './App.css';
 
-function search(typeanything){
+
+//Dictionary named alphaToMorse
+const alphaToMorse = {
+    'a':".-",
+    'b':"-...",
+    'c':"-.-.",
+    'd':"-..",
+    'e':".",
+    'f':"..-.",
+    'g':"--.",
+    'h':"....",
+    'i':"..",
+    'j':".---",
+    'k':"-.-",
+    'l':".-..",
+    'm':"--",
+    'n':"-.",
+    'o':"---",
+    'p':".--.",
+    'q':"--.-",
+    'r':".-.",
+    's':"...",
+    't':"-",
+    'u':"..-",
+    'v':"...-",
+    'w':".--",
+    'x':"-..-",
+    'y':"-.--",
+    'z':"--..",
+    ' ':"/",
+    '0': "-----",
+    '1':".----",
+    '2':"..---",
+    '3':"...--",
+    '4':"....-",
+    '5':".....",
+    '6':"-....",
+    '7':"--...",
+    '8':"---..",
+    '9':"----."
+}
+
+function search(typeAnything){
     let sumMorseCode = '';
-    for(var i = 0; i < typeanything.length; i++){
-        let element = typeanything[i];
-        var result;
-        switch(element){
-            case 'A':
-            case 'a':
-                result = ".-";
-                break;
-            case 'b':
-            case 'B':
-                result = "-...";
-                break;
-            case 'c':
-            case 'C':
-                result = "-.-.";
-                break;
-            case 'd':
-            case 'D':
-                result = "-..";
-                break;
-            case 'e':
-            case 'E':
-                result = ".";
-                break;
-            case 'f':
-            case 'F':
-                result = "..-.";
-                break;
-            case 'g':
-            case 'G':
-                result = "--.";
-                break;
-            case 'h':
-            case 'H':
-                result = "....";
-                break;
-            case 'i':
-            case 'I':
-                result = "..";
-                break;
-            case 'j':
-            case 'J':
-                result = ".---";
-                break;
-            case 'k':
-            case 'K':
-                result = "-.-";
-                break;
-            case 'l':
-            case 'L':
-                result = ".-..";
-                break;
-            case 'm':
-            case 'M':
-                result = "--";
-                break;
-            case 'n':
-            case 'N':
-                result = "-.";
-                break;    
-            case 'o':
-            case 'O':
-                result = "---";
-                break;
-            case 'p':
-            case 'P':
-                result = ".--.";
-                break;
-            case 'q':
-            case 'Q':
-                result = "--.-";
-                break;
-
+    for(var i = 0; i < typeAnything.length; i++){
+        let element = typeAnything[i].toLowerCase();
+        var result = alphaToMorse[element];
+        if(i !== 0){
+            sumMorseCode += ' '; //sumMorseCode = sumMorseCode + ' '
         }
-        sumMorseCode += result;
+        sumMorseCode += result; //sumMorseCode = sumMorseCode + (result or alphaToMorse[element])
     }
     return sumMorseCode;
 }
+
 
 class App extends Component {
     constructor(props){
@@ -87,18 +63,54 @@ class App extends Component {
         this.state = {
             typeSomething: '',
             morseCodeResult: '',
+            typeMorseCode: '',
+            textResult:'',
+            morseToAlpha: this.reverseIt()
         }
+        this.translate = this.translate.bind(this)
     }
+
+    translate(typeMorseCode){
+        var arrayOfStrings = typeMorseCode.split(' '); //"9MorseCode" "7MorseCode"=> the space between 9 and 7 be removed
+        for(let i = 0; i < arrayOfStrings.length; i++){
+            let element = arrayOfStrings[i]; //represents the whole chunk of the character - like ---..
+            let result = this.state.morseToAlpha[element];
+        }
+        return ;
+    }
+    reverseIt(){
+        const result = {};
+        for(var key in alphaToMorse){
+            if(!result.hasOwnProperty(key)){
+                result[alphaToMorse[key]] = key;
+            }
+        }
+        return result;
+    }
+    
 
     render(){
         return(
             <div>
-                <input type="text" id="typeanything" placeholder="Type anything you want here"
+            <h1 className="header">MORSE CODE GAME</h1>
+                <div className="container card">
+                <h4><label for="typeAnything">Type anything in the box below to converts text into Morse code: </label></h4>
+                <input type="text" id="typeAnything" placeholder="Type anything you want here"
                 onChange={(e) => {this.setState({typeSomething: e.target.value})}}/>
                 <button onClick={() => {
                     this.setState({morseCodeResult: search(this.state.typeSomething)});
-                }}>Enter</button>
-                <p>Morse Code Result: {this.state.morseCodeResult}</p>
+                }}>See the outcome of Morse Code</button>
+                <h4><p>Morse Code Result: {this.state.morseCodeResult}</p></h4>
+
+                <h4><label for="typeMorseCode">Reverse - Type the Morse code to translate into text </label></h4>
+                <input type="text" id="typeMorseCode" placeholder="Type Morse Code you want here"
+                onChange={(e) => {this.setState({typeMorseCode: e.target.value})}}/>
+                <button onClick={() => {
+                    this.setState({textResult: search(this.state.typeMorseCode)});
+                }}>See what does it means</button>
+                <h4><p>Text Result: {this.state.textResult}</p></h4>
+                </div>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/International_Morse_Code.svg/1200px-International_Morse_Code.svg.png"/>
             </div>
         );
     }
