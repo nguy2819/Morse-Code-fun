39dba98cdd163a566efe8657b511e607f6262013
Tien Borland <nguy2819@crk.umn.edu>
2018-09-20 10:15:51 -0600

    tried to apply text-to-speech code into this product to read morsecode but it does not work so far because morsecodes are not English language or at least, not a language to read

--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import allVoices from './voice';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+const msg = new SpeechSynthesisUtterance();
+msg.volume = 1; // 0 to 1
+msg.rate = 1; // 0.1 to 10
+msg.pitch = 1; //0 to 2
+msg.text = ".... . -.--";
+
+const voice = allVoices[4] // 47 total
+console.log(`voice: ${voice.name}, language: ${voice.lang}`)
+msg.voiceURI = voice.voiceURI;
+msg.lang = voice.lang;
+
+speechSynthesis.speak(msg);
---
+++ src/voice.js
@@ -0,0 +1,190 @@
+module.exports = [
+    {
+      "name": "Alex",
+      "lang": "en-US"
+    },
+    {
+      "name": "Alice",
+      "lang": "it-IT"
+    },
+    {
+      "name": "Alva",
+      "lang": "sv-SE"
+    },
+    {
+      "name": "Amelie",
+      "lang": "fr-CA"
+    },
+    {
+      "name": "Anna",
+      "lang": "de-DE"
+    },
+    {
+      "name": "Carmit",
+      "lang": "he-IL"
+    },
+    {
+      "name": "Damayanti",
+      "lang": "id-ID"
+    },
+    {
+      "name": "Daniel",
+      "lang": "en-GB"
+    },
+    {
+      "name": "Diego",
+      "lang": "es-AR"
+    },
+    {
+      "name": "Ellen",
+      "lang": "nl-BE"
+    },
+    {
+      "name": "Fiona",
+      "lang": "en"
+    },
+    {
+      "name": "Fred",
+      "lang": "en-US"
+    },
+    {
+      "name": "Ioana",
+      "lang": "ro-RO"
+    },
+    {
+      "name": "Joana",
+      "lang": "pt-PT"
+    },
+    {
+      "name": "Jorge",
+      "lang": "es-ES"
+    },
+    {
+      "name": "Juan",
+      "lang": "es-MX"
+    },
+    {
+      "name": "Kanya",
+      "lang": "th-TH"
+    },
+    {
+      "name": "Karen",
+      "lang": "en-AU"
+    },
+    {
+      "name": "Kyoko",
+      "lang": "ja-JP"
+    },
+    {
+      "name": "Laura",
+      "lang": "sk-SK"
+    },
+    {
+      "name": "Lekha",
+      "lang": "hi-IN"
+    },
+    {
+      "name": "Luca",
+      "lang": "it-IT"
+    },
+    {
+      "name": "Luciana",
+      "lang": "pt-BR"
+    },
+    {
+      "name": "Maged",
+      "lang": "ar-SA"
+    },
+    {
+      "name": "Mariska",
+      "lang": "hu-HU"
+    },
+    {
+      "name": "Mei-Jia",
+      "lang": "zh-TW"
+    },
+    {
+      "name": "Melina",
+      "lang": "el-GR"
+    },
+    {
+      "name": "Milena",
+      "lang": "ru-RU"
+    },
+    {
+      "name": "Moira",
+      "lang": "en-IE"
+    },
+    {
+      "name": "Monica",
+      "lang": "es-ES"
+    },
+    {
+      "name": "Nora",
+      "lang": "nb-NO"
+    },
+    {
+      "name": "Paulina",
+      "lang": "es-MX"
+    },
+    {
+      "name": "Samantha",
+      "lang": "en-US"
+    },
+    {
+      "name": "Sara",
+      "lang": "da-DK"
+    },
+    {
+      "name": "Satu",
+      "lang": "fi-FI"
+    },
+    {
+      "name": "Sin-ji",
+      "lang": "zh-HK"
+    },
+    {
+      "name": "Tessa",
+      "lang": "en-ZA"
+    },
+    {
+      "name": "Thomas",
+      "lang": "fr-FR"
+    },
+    {
+      "name": "Ting-Ting",
+      "lang": "zh-CN"
+    },
+    {
+      "name": "Veena",
+      "lang": "en-IN"
+    },
+    {
+      "name": "Victoria",
+      "lang": "en-US"
+    },
+    {
+      "name": "Xander",
+      "lang": "nl-NL"
+    },
+    {
+      "name": "Yelda",
+      "lang": "tr-TR"
+    },
+    {
+      "name": "Yuna",
+      "lang": "ko-KR"
+    },
+    {
+      "name": "Yuri",
+      "lang": "ru-RU"
+    },
+    {
+      "name": "Zosia",
+      "lang": "pl-PL"
+    },
+    {
+      "name": "Zuzana",
+      "lang": "cs-CZ"
+    }
+  ]
